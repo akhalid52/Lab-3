72f4f5d164b66d24d8a42e603c389e724c4a5745
akhalid52 <156462992+akhalid52@users.noreply.github.com>
2024-02-12 20:45:51 -0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -2,9 +2,3 @@ def write_name_to_file():
     name = "Abdulmanan Khalid"
     with open("name.txt", "w") as file:
         file.write(name)
-
-def helloWorld():
-	print(‘Hello World’)
-
-
-helloWorld()
